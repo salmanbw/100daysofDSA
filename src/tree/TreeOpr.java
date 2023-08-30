0915db3bfc28bf17bce0f24b0577af2c92dccf7e
Salman Bootwala <salmanbw@gmail.com>
2023-08-30 15:09:33 +0400

    Day6: adding Tree operations

--- src/tree/TreeOpr.java
+++ src/tree/TreeOpr.java
@@ -19,7 +19,8 @@ public class TreeOpr {
         TreeNode b = new TreeNode(3);
         TreeNode c = new TreeNode(4);
         TreeNode d = new TreeNode(5);
-        root.left = a; root.right = b; a.left = c; b.right = d;
+        TreeNode e = new TreeNode(6);
+        root.left = a; root.right = b; a.left = c; b.right = d; d.left = e;
 
         preOrder(root);
         TreeNode mirror = mirrorTree(root);
@@ -27,6 +28,26 @@ public class TreeOpr {
         preOrder(mirror);
         System.out.println();
         inOrder(mirror);
+        System.out.println();
+        System.out.println("Height of tree "+heightTree(root));
+        System.out.println("diameter of tree "+diameterTree(root));
+        System.out.println("if identical "+ifIdentical(root,a));
+    }
+
+    public static int LCA(TreeNode root, int num1, int num2){
+        return -1;
+    }
+
+
+
+    public static boolean ifMirror(TreeNode root1, TreeNode root2){
+        if(root1 == null && root2 == null)
+            return true;
+
+        if(root1 == null || root2 == null)
+            return false;
+
+        return root1.data == root2.data && ifMirror(root1.left,root2.right) && ifMirror(root1.right, root2.left);
     }
 
     public static TreeNode mirrorTree(TreeNode root){
@@ -40,11 +61,34 @@ public class TreeOpr {
         return root;
     }
 
+    public static boolean ifIdentical(TreeNode root1,TreeNode root2){
+        if(root1 == null && root2 == null)
+            return true;
+
+        if(root1.data == root2.data)
+            return true;
+
+        if(root1.data != root2.data)
+            return false;
+
+        return ifIdentical(root1.left,root2.left) && ifIdentical(root1.right, root2.right);
+    }
+
     public static int heightTree(TreeNode root){
         if(root == null)
             return 0;
 
         return 1 + Math.max(heightTree(root.left), heightTree(root.right));
+    }
+
+    public static int diameterTree(TreeNode root){
+        if(root == null)
+            return 0;
+
+        int left =  heightTree(root.left);
+        int right = heightTree(root.right);
+
+        return 1 + left + right;
     }
 
     public static int sizeOfTree(TreeNode root){
@@ -77,7 +121,4 @@ public class TreeOpr {
         postOrder(root.right);
         System.out.print(root.data+" ");
     }
-
-
-
 }
